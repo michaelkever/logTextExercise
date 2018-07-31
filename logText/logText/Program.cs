3621e3cd11cb58c5a50150f0d008163495f4af89
Michael Kever <michaelkever@gmail.com>
2018-07-31 10:29:13 -0500

    Exercise Logging Text and Retrieving
    For Tech Academy

---
+++ logText/logText/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace logText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string userInput = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\micha\OneDrive\Desktop\TextDocs\logText.txt", userInput);
+            Console.WriteLine("The number you logged:");
+            string userText = File.ReadAllText(@"C:\Users\micha\OneDrive\Desktop\TextDocs\logText.txt" );
+            Console.WriteLine(userText);
+            Console.ReadLine();
+        }
+    }
+}
